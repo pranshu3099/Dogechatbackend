dfbe07f6e29046bd10495676d8dea65403e2c5e1
pranshu <srivastavapranshu2@gmail.com>
2024-02-12 21:44:54 +0530

    added regex validation for email

--- Regex/regex.js
+++ Regex/regex.js
@@ -1,7 +1,9 @@
 const mobileRegex = /^\d{10}$/;
 const otpRegex = /^\d{4}$/;
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 module.exports = {
   mobileRegex,
   otpRegex,
+  emailRegex,
 };
--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,4 @@
-const { mobileRegex, otpRegex } = require("../Regex/regex");
+const { mobileRegex, emailRegex, otpRegex } = require("../Regex/regex");
 
 const { z, string } = require("zod");
 
@@ -8,6 +8,7 @@ const registerSchema = z.object({
     .min(10)
     .max(10)
     .regex(mobileRegex, { message: "Invalid mobile number" }),
+  email: string().max(100).regex(emailRegex, { message: "Invalid email" }),
 });
 
 const verifyotplength = z.object({
@@ -21,8 +22,13 @@ const validateMobilenumber = z.object({
   mobile_number: string().min(10).max(10).regex(mobileRegex),
 });
 
+const validateEmail = z.object({
+  email: string().max(100).regex(emailRegex, { message: "Invalid email" }),
+});
+
 module.exports = {
   registerSchema,
   verifyotplength,
   validateMobilenumber,
+  validateEmail,
 };
